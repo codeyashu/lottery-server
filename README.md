a6f7f29a3637ecf28d247a183f73d1256de8b382
yashas <yashasycsc@outlook.com>
2018-04-21 11:39:04 +0530

    updated readme and lottery.js

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-React server to interact with lottery contract.
+# React server to interact with lottery contract.
+
+Front end to interact with lottery-contract. The users can send some ether and enter into contention. It can also show the players who have entered. Finally winner can be pseudorandomly picked.
+
+## How to use
+
+- First look at __lottery-contract__ repo to know more about the contract and also to deploy your own contract.
+- Clone the lottery-server repository.
+- Run `yarn` or `yarn install`
+- Edit _lottery.js_ file replace the contract address and abi with your deployed contract.
+- Run `yarn start`
+- Open http://localhost:3000.  
--- src/lottery.js
+++ src/lottery.js
@@ -5,9 +5,28 @@ import web3 from './web3';
 // Copy contract interface (ABI) and contract address from
 // deployed lottery contract and paste in here
 
-const address = ' ';
+const address = '0x4EC19CF2bDF45AF840EF3362E160fA2b75487988';
 
-const abi = [];
+const abi = [
+    {
+        "constant": true, "inputs": [], "name": "manager", "outputs": [{ "name": "", "type": "address" }], "payable": false, "stateMutability": "view", "type": "function"
+    },
+    {
+        "constant": false, "inputs": [], "name": "pickWinner", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function"
+    },
+    {
+        "constant": true, "inputs": [], "name": "getPlayers", "outputs": [{ "name": "", "type": "address[]" }], "payable": false, "stateMutability": "view", "type": "function"
+    },
+    {
+        "constant": false, "inputs": [], "name": "enter", "outputs": [], "payable": true, "stateMutability": "payable", "type": "function"
+    },
+    {
+        "constant": true, "inputs": [{ "name": "", "type": "uint256" }], "name": "players", "outputs": [{ "name": "", "type": "address" }], "payable": false, "stateMutability": "view", "type": "function"
+    },
+    {
+        "inputs": [], "payable": false, "stateMutability": "nonpayable", "type": "constructor"
+    }
+];
 
 // Use address and abi to create local contract instance using web3 library
 // This local copy acts as abstraction of deployed contract
